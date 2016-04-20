0c14fff50f65461b86f295294606be99951f9bb4
dontry <mccoy018@foxmail.com>
2016-04-20 11:03:24 +0800

    完成下载页面,提取css公共部分

--- entry2.js
+++ entry2.js
@@ -5,4 +5,4 @@ require('./node_modules/bootstrap/dist/js/bootstrap.min.js')
 require('./js/app2.js');
 require('./node_modules/bootstrap/dist/css/bootstrap.min.css')
 // require('./node_modules/fullpage.js/jquery.fullPage.css')
-require('./css/examples.css');
+require('./css/common.css');
