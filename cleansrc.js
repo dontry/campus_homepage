7f2776248b24787c1c5c68cc8de9f749e83e4519
dontry <mccoy018@gmail.com>
2016-11-25 14:01:34 +1100

    commit gh-pages

--- cleansrc.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp'),
-    del = require('del'),
-    config = require('../config').cleansrc;
-
-gulp.task('cleansrc', function() {
-    return del(config.src);
-});
--- entry2.js
+++
@@ -1,9 +0,0 @@
-jQuery = require('jquery');
-$ = require('jquery');
-// bootstrap = require('bootstrap');
-require('./node_modules/bootstrap/dist/js/bootstrap.min.js')
-require('./js/app2.js');
-require('./node_modules/bootstrap/dist/css/bootstrap.min.css')
-// require('./node_modules/fullpage.js/jquery.fullPage.css')
-require('./css/common.css');
-// require('./css/index.css');
